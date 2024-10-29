170a76efe5ad71ba4c0463e28e8b50a172bbeb61
welder <welderfox@gmail.com>
2024-10-29 20:21:44 -0300

    Jsx intro

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import ReactDOM from 'react-dom'
+import React from 'react'
 
-const el = document.getElementById('root')
-ReactDOM.render('OLA MUNDO React!!!', el)
+//const el = document.getElementById('root')
+ReactDOM.render(
+    <div>
+        <strong>OLA MUNDO React !!!</strong>
+    </div>,
+    document.getElementById('root'))
