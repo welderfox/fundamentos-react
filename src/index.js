c74f454ad5d68e82e01d23b72688909698d92430
welder <welderfox@gmail.com>
2024-11-06 21:09:45 -0300

    Component-Fragmento

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 //const el = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <Primeiro></Primeiro>
+        <Primeiro />
+        <ComParametro
+            titulo="segundo component"
+            subtitulo="Legal" />
+        <Fragmento></Fragmento>
     </div>,
     document.getElementById('root')
 )
